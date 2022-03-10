72d31b99e65cbf1f8ce8cff40d550bc5562a0c14
coli98 <marcin.balchanowski@gmail.com>
2022-03-10 13:27:39 +0100

    Object question, new Activity with questions, setting UI for Questions

---
+++ app/src/main/java/com/coli98/quizapp/Constants.kt
@@ -0,0 +1,107 @@
+package com.coli98.quizapp
+
+object Constants {
+
+    fun GetQuestions():ArrayList<Question> {
+    val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina","Australia",
+            "Austria", "Armenia",
+            1
+        )
+        questionList.add(que1)
+
+        val que2 = Question(
+            2, "What map in csgo is it?",
+            R.drawable.ic_inferno_map,
+            "de_dustII","de_mirage",
+            "de_inferno", "de_nuke",
+            3
+        )
+        questionList.add(que2)
+
+        val que3 = Question(
+            3, "What sport is it?",
+            R.drawable.ic_ski,
+            "iceskating", "snowboarding",
+            "skydiving", "skiing",
+            4
+        )
+        questionList.add(que3)
+
+        val que4 = Question(
+            4, "What region in Austria is it?",
+            R.drawable.ic_tyrol,
+            "Salzburg", "Tyrol",
+            "Lower Austria", "Upper Austria",
+        2
+        )
+
+        questionList.add(que4)
+
+        val que5 = Question(
+            5, "What is the name of this car brand?",
+            R.drawable.ic_koeningsegg,
+            "Maseratti", "Lamborghini",
+            "Bugatti", "Koeningsegg",
+            4
+        )
+
+        questionList.add(que5)
+
+        val que6 = Question(
+            6, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany", "Georgia",
+            "Greece", "none of these", 1
+        )
+
+        questionList.add(que6)
+
+
+        val que7 = Question(
+            7, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica", "Egypt",
+            "Denmark", "Ethiopia", 3
+        )
+
+        questionList.add(que7)
+
+
+        val que8 = Question(
+            8, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Ireland", "Iran",
+            "Hungary", "India", 4
+        )
+
+        questionList.add(que8)
+
+
+        val que9 = Question(
+            9, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia", "New Zealand",
+            "Tuvalu", "United States of America", 2
+        )
+
+        questionList.add(que9)
+
+
+        val que10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait", "Jordan",
+            "Sudan", "Palestine", 1
+        )
+        questionList.add(que10)
+
+
+        return questionList
+    }
+
+}
---
+++ app/src/main/java/com/coli98/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,60 @@
+package com.coli98.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+
+        val questionList = Constants.GetQuestions()
+        Log.i("QuestionList size is", "${questionList.size}")
+
+        for(i in questionList){
+
+            Log.e("Questions", i.question)
+        }
+
+        var currentPosition = 1
+        val question : Question= questionList[currentPosition - 1] //to nicer viewing the number of questions
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        ivImage?.setImageResource(question.image)
+        tvQuestion?.text = question.question
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+    }
+}
